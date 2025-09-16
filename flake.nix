118a39e2318d54bce32d75df9a5caea553067c97
nilsblix <nilsblix06@gmail.com>
2025-09-16 21:36:03 +0200

    removed nix-darwin comment in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-        # nix-darwin.url = "github:LnL7/nix-darwin/master";
         nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
         nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
