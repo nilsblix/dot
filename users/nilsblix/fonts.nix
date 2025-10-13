347404de8211cc34ab33dbcef61bed979eb14e34
nilsblix <nilsblix06@gmail.com>
2025-10-13 21:04:30 +0200

    Glowstick

--- users/nilsblix/fonts.nix
+++ users/nilsblix/fonts.nix
@@ -5,6 +5,7 @@
             nerd-fonts.hack
             nerd-fonts.jetbrains-mono
             nerd-fonts.fira-code
+            nerd-fonts.iosevka
             inputs.sf-mono-nf.packages.${pkgs.system}.default
             inputs.monaco-nf.packages.${pkgs.system}.monaco
         ];
